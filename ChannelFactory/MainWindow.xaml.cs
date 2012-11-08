1bb69cd1265c12d96e69d0a0b8ea056dd5920e25
Alexander Klee <aklee@catfp.ch>
2012-11-08 18:48:07 +0100

    minor refactorings

--- ChannelFactory/MainWindow.xaml.cs
+++ ChannelFactory/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System.Diagnostics;
 using System.Windows;
 using Configuration;
+using Configuration.Properties;
 using Microsoft.Windows.Controls.Ribbon;
 using TheCodeKing.Net.Messaging;
-using Configuration.Properties;
 
 namespace ChannelFactory
 {
@@ -24,19 +24,24 @@ namespace ChannelFactory
             listener = channel.CreateListener(listener_MessageReceived);
         }
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start(Settings.Default.FilePath_SampleProcess);
+            broadcast.SendToChannel(Settings.Default.ChannelName_Commands, "hello");
         }
 
-        private void Button2_Click(object sender, RoutedEventArgs e)
+        private void ButtonShutdown_Click(object sender, RoutedEventArgs e)
         {
             broadcast.SendToChannel(Settings.Default.ChannelName_Commands, "shutdown");
         }
 
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Settings.Default.FilePath_SampleProcess);
+        }
+
         private void listener_MessageReceived(object sender, XDMessageEventArgs e)
         {
-            MessageBox.Show("Listener closed");
+            MessageBox.Show(string.Format("Received a message on channel {0}: {1}",e.DataGram.Channel, e.DataGram.Message));
         }
     }
 }
--- Process/Program.cs
+++ Process/Program.cs
@@ -1,48 +1,11 @@
 ﻿using System;
 using System.Threading;
 using Configuration;
-using TheCodeKing.Net.Messaging;
 using Configuration.Properties;
+using TheCodeKing.Net.Messaging;
 
 namespace Process
 {
-    public class SomeClass
-    {
-        private IXDBroadcast broadcast;
-
-        private IXDListener listener;
-
-        public SomeClass()
-        {
-        }
-
-        public bool ShutdownRequested { get; set; }
-
-        public void Start()
-        {
-            Console.WriteLine(string.Format("Register Channel '{0}'",Settings.Default.ChannelName_Commands));
-            var channel = XDChannelFactory.GetLocalChannel(Settings.Default.ChannelName_Commands);
-            broadcast = channel.CreateBroadcast();
-            listener = channel.CreateListener(MessageReceived);
-        }
-
-        internal void Close()
-        {
-            broadcast.SendToChannel(Settings.Default.ChannelName_Status, "closing");
-        }
-
-        private void MessageReceived(object sender, XDMessageEventArgs e)
-        {
-            Console.WriteLine(string.Format("Received message {0}", e.DataGram.Message));
-            switch (e.DataGram.Message)
-            {
-                case "shutdown":
-                    ShutdownRequested = true;
-                    break;
-            }
-        }
-    }
-
     public class Program
     {
         private static bool run = true;
@@ -60,6 +23,41 @@ namespace Process
             }
 
             someProcess.Close();
+        }
+    }
+
+    public class SomeClass
+    {
+        private IXDBroadcast broadcast;
+        private int instanceNr;
+        private IXDListener listener;
+
+        public bool ShutdownRequested { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine(string.Format("Register Channel '{0}'", Settings.Default.ChannelName_Commands));
+            var channel = XDChannelFactory.GetLocalChannel(Settings.Default.ChannelName_Commands);
+            broadcast = channel.CreateBroadcast();
+            listener = channel.CreateListener(MessageReceived);
+            broadcast.SendToChannel(Settings.Default.ChannelName_Status, "Process started!");
+        }
+
+        internal void Close()
+        {
+            broadcast.SendToChannel(Settings.Default.ChannelName_Status, "closing");
+        }
+
+        private void MessageReceived(object sender, XDMessageEventArgs e)
+        {
+            Console.WriteLine(string.Format("Received message {0}", e.DataGram.Message));
+            switch (e.DataGram.Message)
+            {
+                case "shutdown":
+                    ShutdownRequested = true;
+                    Console.WriteLine("Shutting down!");
+                    break;
+            }
         }
     }
 }
